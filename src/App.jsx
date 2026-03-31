76db0833022a240625b5086384bc721f8d41b96f
Miftah Adem <miftahaadem@gmail.com>
2026-03-31 18:01:58 +0300

    Modified App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,8 @@ export const App = () => (
   <>
     <Sum a={2} b={3} />
     <Sum a={-5} b={5} />
-    <Sum a={10} b={0} />
-    <Sum a={0} b={5} />
-    <Sum a={0} b={0} />
-    {/* Replace paragraphs with Sum componets */}
-    {/* And remove commented lines :) */}
+    <Sum a={10} />
+    <Sum b={5} />
+    <Sum />
   </>
 );
